c822d5dda289d769cb7d42a6dd3a82a2f3aeb9b4
Ted Mundy <ted.mundy.main@gmail.com>
2023-05-02 11:49:15 +0100

    add placeholder page

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="w-screen h-screen flex flex-col items-center justify-center gap-4">
+    <img src="/img/euphoria.png" class="max-w-lg w-[90%] rounded-xl drop-shadow-lg" />
+    <h1 class="uppercase text-xs text-center text-neutral-400">
+      Page coming soon -
+      <a href="https://github.com/ted-mundy/mundy.io" class="text-neutral-500 hover:text-neutral-600 transition-colors">
+        check out the source code
+      </a>
+    </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  app: {
+    head: {
+      bodyAttrs: {
+        class: 'bg-gray-[#e0e0e0]',
+      },
+      title: 'mundy.io'
+    }
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
