5e169b7a6cafd790748c1e0764c78496adb1c919
Ted Mundy <ted@mundy.io>
2024-06-24 13:35:11 +0100

    feat(web): start on web re-design

--- app.vue
+++ app.vue
@@ -1,11 +1,51 @@
 <template>
-  <div class="w-screen h-screen flex flex-col items-center justify-center gap-4">
-    <img src="/img/euphoria.png" class="max-w-lg w-[90%] rounded-xl drop-shadow-lg" />
-    <h1 class="uppercase text-xs text-center text-neutral-400">
-      Page coming soon -
-      <a href="https://github.com/ted-mundy/mundy.io" class="text-neutral-500 hover:text-neutral-600 transition-colors">
-        check out the source code
+   <div class="text-center mx-auto max-w-3xl px-4">
+    <h1 class="font-bold text-4xl mt-20">
+      <a href="https://github.com/ted-mundy/">
+        ted mundy
       </a>
     </h1>
-  </div>
+    <h4 class="italic text-2xl">
+      software engineer
+    </h4>
+    <p class="mt-2 font-bold">
+      you can send me an email <a href="mailto:ted@mundy.io">here,</a> or find me on
+      <a href="https://github.com/ted-mundy/">
+        github.
+      </a>
+    </p>
+    <p class="mt-6">
+      software engineer with a passion for building and scaling web applications. currently @ <a href="https://acris.co.uk">acris.</a>
+    </p>
+    <hr class="mt-8" />
+    <h1 class="text-xl mt-8 font-bold text-left">
+      projects
+    </h1>
+    <ul class="text-left w-fit mx-auto px-12 mt-4">
+      <li class="mt-2">
+        <a href="https://portal.wetradetech.com">
+          <code>WeTradeTech</code>
+        </a>:
+        online portal for clients to manage bookings. (closed source, for employer)
+      </li>
+      <li class="mt-2">
+        <a href="https://jobdip.com/app">
+          <code>jobdip</code>
+        </a>:
+        application for freelancers to find jobs for their schedule. (closed source, for employer)
+      </li>
+      <li class="mt-2">
+        <a href="https://cliphog.com">
+          <code>cliphog</code>
+        </a>:
+        video creation platform for creators (closed source)
+      </li>
+      <li class="mt-2">
+        <a href="https://github.com/ted-mundy/messaging-app">
+          <code>messaging-app</code>
+        </a>:
+        open source messaging app with rust, react and go
+      </li>
+    </ul>
+   </div>
 </template>
